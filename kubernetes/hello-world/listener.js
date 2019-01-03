ef34b9a2c8de6298f55dac2033b456e60e697743
waldoaraque <waldoaraque10@gmail.com>
2019-01-03 12:08:04 -0400

    Adding kubernetes folder to deploy a lot of hello worlds

---
+++ kubernetes/hello-world/listener.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("¡Hello World!");
+}
+const www = http.createServer(handleRequest);
+www.listen(8080);
