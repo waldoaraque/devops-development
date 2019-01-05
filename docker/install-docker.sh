e9106d67eddb2fdd27555c8f0b3ef0dfe22b2255
waldoaraque <waldoaraque10@gmail.com>
2019-01-05 17:54:42 -0400

    Making better the script to install docker aplication engine

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,8 +1,27 @@
-sudo apt-get remove docker docker-engine docker.io
+#remove docker olds versions 
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
 
-sleep 3
+#some requirements
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+#add the repository to the machine
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 sudo apt-get update \
   && sudo apt-get install -qy docker.io
 
-sleep 3
+sudo docker -v
+
+#test the container engine
+sudo docker run hello-world
