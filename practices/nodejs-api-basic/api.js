8fe1a23ec20be7c0acc531477b03e50e3f53c61d
waldoaraque <waldoaraque10@gmail.com>
2019-01-07 15:46:19 -0400

    Adding the response get and post method to the api, using marvelCharacter for this practice

--- practices/nodejs-api-basic/api.js
+++ practices/nodejs-api-basic/api.js
@@ -9,8 +9,17 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => {
-	res.send({message: `Hello World`});
+app.get('/api/marvel', (req, res) => {
+	res.send(200, {marvelCharacter: []});
+});
+
+app.get('/api/marvel/:marvelCharacterId', (req, res) => {
+	
+});
+
+app.post('/api/marvel', (req, res) => {
+	console.log(req.body);
+	res.status(200).send({message: 'The marvelCharacter it does not exist'});
 });
 
 app.listen(port, () => {
