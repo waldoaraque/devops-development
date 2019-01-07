7b0ec193d04a9d3aa435a1b30867dc685309c3a7
waldoaraque <waldoaraque10@gmail.com>
2019-01-07 19:00:08 -0400

    Reinitialized the template from create-react-app to practice

---
+++ practices/express+socket-practice/socket-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import socketIOClient from 'socket.io-client';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>Making sure this works</p>
+      </div>
+    );
+  }
+}
+
+export default App;
