c02b3e2d69c09f2bbbbd681bce7578859f9b0b76
waldoaraque <waldoaraque10@gmail.com>
2019-01-10 14:32:52 -0400

    Refactorizing all the code for the basic API

---
+++ practices/nodejs-api-basic/controllers/controllerHelloWorld.js
@@ -0,0 +1,16 @@
+function getHelloWorld (req, res)
+{
+	let message = {
+		message: 'Hello World'
+	};
+
+	try{
+		res.status(200).send(message);
+		console.log(message);
+	}catch(e){
+		res.send('An error has occurred');
+		console.log(e);
+	}
+}
+
+module.exports = getHelloWorld;
