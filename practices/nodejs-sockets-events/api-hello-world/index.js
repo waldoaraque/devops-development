738d2c9636f1b8c71e147e0a755f11ece1fba150
waldoaraque <waldoaraque10@gmail.com>
2019-01-10 19:04:22 -0400

    function to send a message to the API from the client that is a react app

--- practices/nodejs-sockets-events/api-hello-world/index.js
+++ practices/nodejs-sockets-events/api-hello-world/index.js
@@ -13,15 +13,21 @@ app.use(api);
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3080;
 const io = socketIO(server);
 
+io.on('connection', (socket) => {
+	
+  console.log("Client Successfully Connected");
 
-io.on('connection', socket => {
-  console.log('connected client...');
-  socket.on('disconnect', () => console.log('disconnected client...'));
+  socket.on('message', () =>{
+  	console.log('message has been received');
+  	io.emit('emitiendo','successfully connection');
+  });
+
+  socket.on('disconnect', () => {
+  	console.log('disconnected');
+  });
 });
 
-app.listen(port, () => console.log(`API running at http://localhost:${port}`));
-
-
+app.listen(port, () => console.log(`API running at http://localhost:${port} with status: 200`));
--- practices/nodejs-sockets-events/client-connection/src/App.js
+++ practices/nodejs-sockets-events/client-connection/src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      endpoint: "http://localhost:3080",
+      message: 'holi'
+    };
+  }
+
+  send() {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('emitiendo', this.state.endpoint);
+  }
+
   render() {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.on('message', () => {
+      console.log('message completed');
+    });
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={() => this.send()}> Sen Message to API </button>
       </div>
     );
   }
