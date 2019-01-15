cd3e0a3b96d160c76a5c575304582647642bea4f
waldoaraque <waldoaraque10@gmail.com>
2019-01-15 12:44:11 -0400

    Script to install docker-ce has been completed

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,9 +1,11 @@
+VERSION=18.03.0~ce-0~ubuntu
+
 #remove docker olds versions 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 
 #some requirements
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -12,14 +14,16 @@ sudo apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
+
 #add the repository to the machine
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update \
-  && sudo apt-get install -qy docker.io
+sudo apt-get update && sudo apt-get install -y docker-ce
+
+sudo apt-get install docker-ce=${VERSION}
 
 sudo docker -v
 
