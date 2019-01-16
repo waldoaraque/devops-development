5266d42643fab46bc741ed318db007703e94b310
waldoaraque <waldoaraque10@gmail.com>
2019-01-16 11:45:36 -0400

    Converting the response in a const message to aslo send in a console when i run the app in docker

--- gitlab-ci-cd/index.js
+++ gitlab-ci-cd/index.js
@@ -3,12 +3,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const message = { 
+  		gitlab: "hello world"
+   	};
+
 app.get("/", (req, res) => {
   res
   .status(200)
-  .send({ 
-  	gitlab: "hello world"
-   });
+  .send(message);
 });
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT} \n ${message.gitlab}`));
