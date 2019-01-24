a119931e868c4d57e86915b9e62baca1d358c5c5
waldoaraque <waldoaraque10@gmail.com>
2019-01-24 12:49:39 -0400

    Add other cookbook to configure a loadbalancer for my servers

--- chef/try-chef/cookbook/README.md
+++
@@ -1,1 +0,0 @@
-# Here is my first cookbook with Chef.
--- chef/try-chef/cookbooks/loadbalancer/metadata.rb
+++ chef/try-chef/cookbooks/loadbalancer/metadata.rb
@@ -1,8 +1,8 @@
-name 'cookbook'
+name 'loadbalancer'
 maintainer 'waldoaraque'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures webserver'
-long_description 'Installs/Configures webserver'
+description 'Installs/Configures loadbalancer'
+long_description 'Installs/Configures loadbalancer'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
