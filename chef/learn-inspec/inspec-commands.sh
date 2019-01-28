5245e409327179e67bf14bb901eb2174bceeffb9
waldoaraque <waldoaraque10@gmail.com>
2019-01-28 17:25:58 -0400

    adding more commands for inspec

--- chef/learn-inspec/inspec-commands.sh
+++ chef/learn-inspec/inspec-commands.sh
@@ -5,3 +5,9 @@ inspec exec auditd -t ssh://root:password@target
 #ssh:// is the scheme. It specifies an SSH connection.
 #root:password is the username and password for the account that permits SSH access.
 #target is the hostname of the target system. The Docker image comes preconfigured with this hostname.
+
+inspec check auditd/
+#makes sure that our profile does not have errors.
+
+inspec archive auditd/
+#convert our profile to a zip or tar.gz packaged file
